8a6241f12990c7bde0f4b4157ef0a73f104bf08f
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-09-20 22:07:09 +0000

    installed mocha and chai and adjusted headtest with mocha framework and chai lib

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,22 @@
 const head = require('../head').head;
 const assertEqual = require('../head').assertEqual;
+const assert = require('chai').assert;
 
-assertEqual(head([]), undefined);
-assertEqual(head([5,6,7]), "Elliot");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head testing", ()=> {
+  it('returns 1 for [1,2,3]', function() {
+    let input = [1,2,3]
+    assert.strictEqual(head(input), 1);
+  })
+  it('returns 5 for [5]', function() {
+    let input = [5]
+    assert.strictEqual(head(input), 5);
+  })
+  it('returns undefined for []', function() {
+    let input = []
+    assert.strictEqual(head(input), undefined);
+  })
+  it('should fail if head[0] does not match', function() {
+    let input = ["Alderson",6,7]
+    assert.notEqual(head(input), "Elliot");
+  })
+});
